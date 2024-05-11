353fb833bfb16ca7b3810a7a4a0ab9ab7694f4c6
alperenatnr <alperenataner10@hotmail.com>
2024-05-11 12:41:56 +0300

    DAO - Controller

--- src/java/Controller/DoctorController.java
+++ src/java/Controller/DoctorController.java
@@ -9,70 +9,79 @@ import java.util.List;
 @Named
 @ViewScoped
 public class DoctorController extends BaseController<Doctor> {
-
-    private Doctor entity;
+    
+    private Doctor doctor;
     private DoctorDAO dao;
     private List<Doctor> list;
-
+    
+    public String create() {
+        
+        this.getDao().createDoctor(this.doctor);
+        
+        return "index.html";
+        
+    }
+    
     public Doctor getEntity() {
-        if (this.entity == null) {
-            this.entity = new Doctor();
+        if (this.doctor == null) {
+            this.doctor = new Doctor();
         }
-        return entity;
+        return doctor;
     }
-
+    
     public void setEntity(Doctor Entity) {
-        this.entity = Entity;
+        this.doctor = Entity;
     }
-
+    
     public DoctorDAO getDao() {
-        if(this.dao == null){
+        if (this.dao == null) {
             this.dao = new DoctorDAO();
         }
         return dao;
     }
-
+    
     public void setDao(DoctorDAO dao) {
         this.dao = dao;
     }
-
+    
     public List<Doctor> getList() {
+        this.list = this.getDao().getDoctorList();
         return list;
     }
-
+    
     public void setList(List<Doctor> list) {
         this.list = list;
     }
-
+    
     public DoctorController() {
-
+        
     }
-
+    
     @Override
     public void AddEntity(Doctor doctor) {
         DoctorDAO dao = new DoctorDAO();
         dao.createDoctor(doctor);
-
+        
     }
-
+    
     @Override
     public Doctor GetEntityById(int id) {
         return null;
     }
-
+    
     @Override
     public List<Doctor> GetEntityList() {
         DoctorDAO dao = new DoctorDAO();
         return dao.getDoctorList();
     }
-
+    
     @Override
     public void UpdateEntity(int id) {
-
+        
     }
-
+    
     @Override
     public void DeleteEntity(int id) {
-
+        
     }
 }
